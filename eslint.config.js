c87ee17497babe77cbb36b46a37b3670e90c8762
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2026-02-02 16:33:05 -0300

    feat: adding lint settings

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      indent: ["error", 4],
+      semi: ["error", "never"],
+      quotes: ["error", "double"],
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+];
