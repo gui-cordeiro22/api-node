d0427848ecb0f3fb3c0d3966e59d8ea618448351
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2026-02-02 17:15:35 -0300

    refactor: lint adjustments

--- eslint.config.js
+++ eslint.config.js
@@ -3,22 +3,22 @@ import tseslint from "typescript-eslint";
 import globals from "globals";
 
 export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.node,
-      },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                ...globals.node,
+            },
+        },
+        rules: {
+            indent: ["error", 4],
+            semi: ["error", "always"],
+            quotes: ["error", "double"],
+            "@typescript-eslint/no-unused-vars": "warn",
+        },
     },
-    rules: {
-      indent: ["error", 4],
-      semi: ["error", "never"],
-      quotes: ["error", "double"],
-      "@typescript-eslint/no-unused-vars": "warn",
-    },
-  },
 ];
