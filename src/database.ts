6425409b681bfd9ddfa5a4c38e82a596f862c12e
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2026-02-02 22:06:10 -0300

    feat: adding dot env

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,20 @@
 // Dependencies
+import "dotenv/config";
 import knex from "knex";
+
+console.log(process.env);
 
 // Types
 import { Knex } from "knex";
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL not found");
+}
+
 export const databaseConfig: Knex.Config = {
     client: "sqlite",
     connection: {
-        filename: "./database/app.db",
+        filename: process.env.DATABASE_URL,
     },
     useNullAsDefault: true,
     migrations: {
