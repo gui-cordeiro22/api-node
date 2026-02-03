66fb9759879885735a6ceb047d267f39080fa797
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2026-02-03 14:39:16 -0300

    feat: adding vitest and first test initial structures

---
+++ test/example.test.ts
@@ -0,0 +1,8 @@
+// Dependencies
+import { expect, test } from "vitest";
+
+test("Testing if user can create a transaction", () => {
+    const responseStatusCode = 201;
+
+    expect(responseStatusCode).toEqual(201);
+});
