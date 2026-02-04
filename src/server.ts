1b8422c5b61339bee1d8ce1bb77991955cee22f4
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2026-02-03 21:15:35 -0300

    fix: build adjustments

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,6 @@ import { app } from "./app.js";
 // Utils
 import { env } from "./env/index.js";
 
-app.listen({ port: env.PORT }).then(() => console.log("HTTP Server Running"));
+app.listen({ port: env.PORT, host: "0.0.0.0" }).then(() =>
+    console.log("HTTP Server Running"),
+);
